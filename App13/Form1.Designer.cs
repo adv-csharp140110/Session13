0521110dfcaa657e86c5c9191b9ef9d7791c95d3
Pasha <pashaie@gmail.com>
2023-05-08 18:41:48 +0330

    Add project files.

---
+++ App13/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace App13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLambda = new System.Windows.Forms.Button();
+            this.buttonEmbded = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLambda
+            // 
+            this.buttonLambda.Location = new System.Drawing.Point(12, 12);
+            this.buttonLambda.Name = "buttonLambda";
+            this.buttonLambda.Size = new System.Drawing.Size(75, 23);
+            this.buttonLambda.TabIndex = 0;
+            this.buttonLambda.Text = "Lambda";
+            this.buttonLambda.UseVisualStyleBackColor = true;
+            this.buttonLambda.Click += new System.EventHandler(this.buttonLambda_Click);
+            // 
+            // buttonEmbded
+            // 
+            this.buttonEmbded.Location = new System.Drawing.Point(364, 12);
+            this.buttonEmbded.Name = "buttonEmbded";
+            this.buttonEmbded.Size = new System.Drawing.Size(139, 23);
+            this.buttonEmbded.TabIndex = 1;
+            this.buttonEmbded.Text = "Embede DB";
+            this.buttonEmbded.UseVisualStyleBackColor = true;
+            this.buttonEmbded.Click += new System.EventHandler(this.buttonEmbded_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(364, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(510, 150);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 506);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonEmbded);
+            this.Controls.Add(this.buttonLambda);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLambda;
+        private System.Windows.Forms.Button buttonEmbded;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ App13/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLambda_Click(object sender, EventArgs e)
+        {
+            //Lambda Expression
+            //Arrow function
+            //lazy
+
+            var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result1 = new List<int>();
+            foreach (int i in arr)
+            {
+                result1.Add(i * 10);
+            }
+            //
+            var result1JADID = MyMap(arr, x => x * 10);
+            var result1Linq =arr.Select(x => x * 10);
+
+            var result2 = new List<int>();
+            foreach (int i in arr)
+            {
+                result2.Add(i * i);
+            }
+            var result2JADID = MyMap(arr, x => x * x);
+        }
+
+        delegate int MyLambda(int a);
+        private List<int> MyMapDelegate(int[] arr, MyLambda fnc)
+        {
+            var result = new List<int>();
+            foreach (int i in arr)
+            {
+                var x = fnc(i);
+                result.Add(x);
+            }
+            return result;
+
+        }
+
+        private List<int> MyMap(int[] arr, Func<int, int> fnc)
+        {
+            var result = new List<int>();
+            foreach (int i in arr)
+            {
+                var x = fnc(i);
+                result.Add(x);
+            }
+            return result;
+        }
+
+        private void buttonEmbded_Click(object sender, EventArgs e)
+        {
+            //Embded
+            // Sqlite
+            // SQL Server Compact Edition CE
+            // ...
+
+            // ADO.net
+            // ORM > Entity Framework
+
+            var conectionStr = "URI=file:app.db";
+            using (var connection = new SQLiteConnection(conectionStr))
+            {
+                var command = new SQLiteCommand("select * from product", connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                var result = new List<Product>();
+                while (reader.Read())
+                {
+                    result.Add(new Product { 
+                        Id = Convert.ToInt32(reader["id"]), 
+                        Name = reader["name"] as string, 
+                        Price = Convert.ToInt32(reader["price"]) });
+                }
+                dataGridView1.DataSource = result;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public int Price { get; set; }
+    }
+}
